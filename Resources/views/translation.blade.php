331c4e5445fc1dc42dd758365d8d7045beff7ae3
Niels Peen <niels.peen@wanwire.com>
2025-08-21 20:50:16 -0500

    css updates

--- Resources/views/translation.blade.php
+++ Resources/views/translation.blade.php
@@ -1,5 +1,6 @@
 <div class="margin-bottom">
     <div class="alert alert-ai-translation">
-        <?php echo nl2br(htmlspecialchars($translation)) ?>
+       <div class="alert-ai-translation-title" >{{ __('AI Translation') }}</div>
+       {{  nl2br(htmlspecialchars($translation)) }}
     </div>
 </div>
