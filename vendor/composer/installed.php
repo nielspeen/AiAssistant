228fb7b8ad5634f705bef38d67e657b10a92ffa6
Niels Peen <niels.peen@wanwire.com>
2025-08-26 08:05:36 -0500

    improved language detection

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nielspeen/ai-assistant',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c5e15810e8b657d3760beca9ad8686f42db3f693',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nielspeen/ai-assistant' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c5e15810e8b657d3760beca9ad8686f42db3f693',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'patrickschur/language-detection' => array(
+            'pretty_version' => 'v5.3.1',
+            'version' => '5.3.1.0',
+            'reference' => 'df8d32021b2ef9fde52e6fcccb83e3806822c9c6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../patrickschur/language-detection',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
